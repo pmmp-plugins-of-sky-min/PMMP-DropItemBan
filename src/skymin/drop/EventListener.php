31f0b1672a435de9d9a51788152386493650e317
sky-min <81374952+sky-min@users.noreply.github.com>
2021-09-05 13:47:49 +0900

    update EventListener.php

--- src/skymin/drop/EventListener.php
+++ src/skymin/drop/EventListener.php
@@ -1,1 +1,26 @@
+<?php
+declare(strict_types = 1);
 
+namespace skymin\drop;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerDropItemEvent;
+
+class EventListener implements Listener{
+	
+	public function __construct(
+		private DropItemSetting $plugin
+	){}
+	
+	public function onDrop(PlayerDropItemEvent $ev) :void{
+		$player = $ev->getPlayer();
+		if($player->isOp()) return;
+		$item = $ev->getItem();
+		if($this->plugin->is_Item($item)){
+			$ev->setCancelled();
+			$player->sendTip('버릴 수 없는 아이템 입니다.');
+		}
+	}
+	
+}
