3b0edbf476d33b3fd2ef2479c46fae7b6f77c1ee
sky-min <81374952+sky-min@users.noreply.github.com>
2021-09-05 13:42:57 +0900

    update DragonDeathEffect.php

--- src/skymin/drop/DragonDeathEffect.php
+++ src/skymin/drop/DragonDeathEffect.php
@@ -1,1 +1,60 @@
+<?php
+declare(strict_types = 1);
 
+namespace skymin\drop;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+use pocketmine\item\Item;
+
+use skymin\drop\command\MainCmd;
+
+use function is_dir;
+use function mkdir;
+
+class DropItemSetting extends PluginBase{
+	
+	private Config $database;
+	
+	public array $db;
+	
+	public function onEnable() :void{
+		$this->database = new Config($this->getDataFolder() . 'Config.yml', Config::YAML, ['item' => []]);
+		$this->db = $this->database->getAll();
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->getServer()->getCommandMap()->register('드롭밴', new MainCmd($this));
+	}
+	
+	public function onDisable() :void{
+		$this->database->setAll($this->db);
+		$this->database->save();
+	}
+	
+	public function addItem(Item $item) :void{
+	  $c = $item->getCount();
+	  $item->setCount(1);
+		$this->db['item'][] = $item->jsonSerialize();
+		$item->setCount($c);
+	}
+	
+	public function reset() :void{
+		unset($this->db['item']);
+		$this->db['item'] = [];
+	}
+	
+	public function is_Item(Item $item) :bool{
+	  $c = $item->getCount();
+	  $item->setCount(1);
+	  $nbt = $item->jsonSerialize();
+		foreach ($this->db['item'] as $key => $value){
+		  if($value === $nbt){
+		    return true;
+		    break;
+		  }
+		}
+		$item->setCount($c);
+		return false;
+	}
+	
+}
